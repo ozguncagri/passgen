c55f4d71ea69147a3a65f00669ff1832f9e27992
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-08-12 21:09:34 +0300

    cobra interation for better cli experience

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "passgen",
+	Short: "Password generator for your daily usage",
+	Long:  "Password generator for your daily usage in all your apps and websites",
+	Run:   passwordGenerator,
+}
+
 func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func passwordGenerator(cmd *cobra.Command, args []string) {
 	appName := askForKeyName()
-
 	password := askForPassword()
-
 	pwdLength := askForPasswordLength()
-
 	pool := askForCharPool()
 
 	generatedPassword := generatePassword(pool, appName, password, pwdLength)
