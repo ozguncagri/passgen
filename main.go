b8c9c848efea4471fc1eba61e6017dba0cd147f1
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-08-12 21:48:06 +0300

    added wallet and their sub-command skeletons

--- main.go
+++ main.go
@@ -14,6 +14,53 @@ var rootCmd = &cobra.Command{
 	Run:   generalPasswordGeneratorCommand,
 }
 
+func init() {
+	generateCommand := &cobra.Command{
+		Use:   "generate",
+		Short: "Password generator for your daily usage",
+		Long:  "Password generator for your daily usage in all your apps and websites",
+		Run:   generalPasswordGeneratorCommand,
+	}
+
+	walletCommand := &cobra.Command{
+		Use:   "wallet",
+		Short: "Lists ",
+		Long:  "Password generator for your daily usage in all your apps and websites",
+		Run:   walletList,
+	}
+
+	walletCommand.AddCommand(
+		&cobra.Command{
+			Use:     "list",
+			Aliases: []string{"ls"},
+			Short:   "Password generator for your daily usage",
+			Long:    "Password generator for your daily usage in all your apps and websites",
+			Run:     walletList,
+		},
+		&cobra.Command{
+			Use:   "add",
+			Short: "Password generator for your daily usage",
+			Long:  "Password generator for your daily usage in all your apps and websites",
+			Run:   walletAdd,
+		},
+		&cobra.Command{
+			Use:     "remove",
+			Aliases: []string{"rm"},
+			Short:   "Password generator for your daily usage",
+			Long:    "Password generator for your daily usage in all your apps and websites",
+			Run:     walletRemove,
+		},
+		&cobra.Command{
+			Use:   "update",
+			Short: "Password generator for your daily usage",
+			Long:  "Password generator for your daily usage in all your apps and websites",
+			Run:   walletUpdate,
+		},
+	)
+
+	rootCmd.AddCommand(generateCommand, walletCommand)
+}
+
 func main() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -30,4 +77,20 @@ func generalPasswordGeneratorCommand(cmd *cobra.Command, args []string) {
 	generatedPassword := generatePassword(pool, appName, password, pwdLength)
 
 	fmt.Printf("\nYour password is : %v\n", generatedPassword)
+}
+
+func walletList(cmd *cobra.Command, args []string) {
+
+}
+
+func walletAdd(cmd *cobra.Command, args []string) {
+
+}
+
+func walletRemove(cmd *cobra.Command, args []string) {
+
+}
+
+func walletUpdate(cmd *cobra.Command, args []string) {
+
 }
