125ab7136c9754dcdd2106a4f673a9fbd61b047e
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-08-13 19:31:32 +0300

    some corrections on code and add some comments

--- generators/generatePassword.go
+++ generators/generatePassword.go
@@ -2,19 +2,23 @@ package generators
 
 import "fmt"
 
+// GeneratePassword function used for generating password with required information
 func GeneratePassword(pool, appName, masterPassword string, length int) string {
-	var asciiNumberSum int64
+	var seedNumber int64
 
+	// combine all required information for password generation seed
 	combination := fmt.Sprintf(
-		"%s%s%s",
+		"%s%s%s%s",
 		sha3StringGenerator(appName),
 		sha3StringGenerator(masterPassword),
 		sha3StringGenerator(string(length)),
+		sha3StringGenerator(pool),
 	)
 
+	// sum all integers correspoding to rune
 	for _, v := range combination {
-		asciiNumberSum += int64(rune(v))
+		seedNumber += int64(rune(v))
 	}
 
-	return string(generateRandomRuneArray(length, pool, asciiNumberSum))
+	return string(generateRandomRuneArray(length, pool, seedNumber))
 }
