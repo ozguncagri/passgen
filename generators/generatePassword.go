a9f96984b1d0868355b3a4b29b92973274c4f6d8
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-08-13 14:59:16 +0300

    changed combination method of input datas

--- generators/generatePassword.go
+++ generators/generatePassword.go
@@ -5,12 +5,12 @@ import "fmt"
 func GeneratePassword(pool, appName, masterPassword string, length int) string {
 	var asciiNumberSum int64
 
-	combination := sha3StringGenerator(fmt.Sprintf(
+	combination := fmt.Sprintf(
 		"%s%s%s",
 		sha3StringGenerator(appName),
 		sha3StringGenerator(masterPassword),
 		sha3StringGenerator(string(length)),
-	))
+	)
 
 	for _, v := range combination {
 		asciiNumberSum += int64(rune(v))
