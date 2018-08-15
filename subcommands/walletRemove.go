67b38fc07c639f56935c4d21995e61f9bd344925
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-08-15 20:08:57 +0300

    Wallet remove items subcommand is implemented

--- subcommands/walletRemove.go
+++ subcommands/walletRemove.go
@@ -1,7 +1,32 @@
 package subcommands
 
-import "github.com/spf13/cobra"
+import (
+	"passgen/config"
 
+	"github.com/spf13/cobra"
+)
+
+// WalletRemove is remotes one ore more items from wallet
 func WalletRemove(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		cmd.Help()
+		return
+	}
 
+	var temp []config.WalletItem
+	for _, v := range config.GlobalConfig.Wallet {
+		isRequestFind := false
+		for _, k := range args {
+			if k == v.Key {
+				isRequestFind = true
+			}
+		}
+
+		if !isRequestFind {
+			temp = append(temp, v)
+		}
+	}
+
+	config.GlobalConfig.Wallet = temp
+	config.Save()
 }
