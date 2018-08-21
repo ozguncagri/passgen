19344c5c460645ad561549809c3208446a5fffa2
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-08-21 12:39:46 +0300

    More colored printer functions added

--- helpers/colorPrinters.go
+++ helpers/colorPrinters.go
@@ -2,6 +2,18 @@ package helpers
 
 import "github.com/fatih/color"
 
+// PositivePrint prints bold and green colored text
+var PositivePrint = color.New(color.FgGreen, color.Bold).PrintFunc()
+
+// NegativePrint prints bold and red colored text
+var NegativePrint = color.New(color.FgRed, color.Bold).PrintFunc()
+
+// PositivePrintf prints bold and green colored text with formatter
+var PositivePrintf = color.New(color.FgGreen, color.Bold).PrintfFunc()
+
+// NegativePrintf prints bold and red colored text with formatter
+var NegativePrintf = color.New(color.FgRed, color.Bold).PrintfFunc()
+
 // PositivePrintln prints bold and green colored text with line ending
 var PositivePrintln = color.New(color.FgGreen, color.Bold).PrintlnFunc()
 
