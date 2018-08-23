031c54eda64ddee27ba2e02389a59125f02108d5
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-08-23 23:40:37 +0300

    Safety check sub-command implemented

--- subcommands/safetyCheck.go
+++ subcommands/safetyCheck.go
@@ -1,8 +1,25 @@
 package subcommands
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+	"passgen/safety"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"gopkg.in/AlecAivazis/survey.v1"
+)
 
 // SafetyCheck runs couple of security checks for your password's safety
 func SafetyCheck(cmd *cobra.Command, args []string) {
+	password := ""
+	prompt := &survey.Password{
+		Message: "What is your password :",
+	}
 
+	err := survey.AskOne(prompt, &password, survey.MinLength(1))
+	if err != nil {
+		os.Exit(1)
+	}
+
+	safety.Check(strings.TrimSpace(password))
 }
