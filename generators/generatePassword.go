32c97f2390432d85f47709bbe7e29aeb62ae5780
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2018-09-10 01:39:01 +0300

    Function name refactored

--- generators/generatePassword.go
+++ generators/generatePassword.go
@@ -5,10 +5,10 @@ import "passgen/helpers"
 // GeneratePassword function used for generating password with required information
 func GeneratePassword(pool, key, masterPassword string, length int) string {
 	seedNumber := helpers.SecureStringCombiner(
-		helpers.SHA3Calculator(key),
-		helpers.SHA3Calculator(masterPassword),
-		helpers.SHA3Calculator(string(length)),
-		helpers.SHA3Calculator(pool),
+		helpers.SHA512Calculator(key),
+		helpers.SHA512Calculator(masterPassword),
+		helpers.SHA512Calculator(string(length)),
+		helpers.SHA512Calculator(pool),
 	)
 
 	return string(generateRandomRuneArray(length, pool, seedNumber))
