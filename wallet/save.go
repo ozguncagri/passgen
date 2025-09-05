333296691ed026d39f4d4201bc7f6c6cb4f5c925
Özgün Çağrı AYDIN <ozguncagri@gmail.com>
2025-09-05 17:48:30 +0300

    Wallet save bug fixed

--- wallet/save.go
+++ wallet/save.go
@@ -5,6 +5,7 @@ import (
 	"encoding/gob"
 	"os"
 	"passgen/helpers"
+	"path/filepath"
 )
 
 // Save function saves marshals and saves global configuration variable to file
@@ -27,8 +28,13 @@ func Save(walletPassword string, memoryWallet *PassgenWallet, walletPath string)
 		return err
 	}
 
+	// Ensure directory exists
+	if err := os.MkdirAll(filepath.Dir(walletPath), 0o600); err != nil {
+		return err
+	}
+
 	// Write encrypted wallet to file
-	err = os.WriteFile(walletPath, encryptedWallet, 0777)
+	err = os.WriteFile(walletPath, encryptedWallet, 0o600)
 	if err != nil {
 		return err
 	}
